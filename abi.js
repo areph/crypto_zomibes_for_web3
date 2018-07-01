64cd49f670ee126939021fc5902215af83417bf4
areph <40balmung@gmail.com>
2018-07-01 23:00:23 +0900

    set git ignore

---
+++ abi.js
@@ -0,0 +1,118 @@
+const abi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_zombieId",
+				"type": "uint256"
+			},
+			{
+				"name": "_targetDna",
+				"type": "uint256"
+			}
+		],
+		"name": "feedAndMultiply",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_zombieId",
+				"type": "uint256"
+			},
+			{
+				"name": "_kittyId",
+				"type": "uint256"
+			}
+		],
+		"name": "feedOnKitty",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "zombies",
+		"outputs": [
+			{
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"name": "dna",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "zombieToOwner",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_name",
+				"type": "string"
+			}
+		],
+		"name": "createRandomZombie",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "zombieId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "dna",
+				"type": "uint256"
+			}
+		],
+		"name": "NewZombie",
+		"type": "event"
+	}
+];
+
+export default abi;
